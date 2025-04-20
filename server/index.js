8dd7995df26b94fd7000a5874147d4266047151d
Bandi Murali Chowdary <muralichowdary386@gmail.com>
2025-04-20 15:02:51 +0530

    Added root route for /

--- server/index.js
+++ server/index.js
@@ -9,13 +9,18 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
 // Test route
 app.get("/api/test", (req, res) => {
   res.json({ message: "Backend is working fine! 🐔" });
+});
+
+// Add a route for the root URL "/"
+app.get("/", (req, res) => {
+  res.send("Welcome to the Poultry Paradise API! 🐔");
 });
 
 app.listen(PORT, () => {
